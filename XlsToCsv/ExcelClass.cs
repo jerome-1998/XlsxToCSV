5c33127e8fa267e1fccb03bba0f08f2fe4128eda
jerome-1998 <jerome.eller9898@gmx.de>
2020-03-01 18:47:38 +0100

    Version 1.0

---
+++ XlsToCsv/ExcelClass.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Excel = Microsoft.Office.Interop.Excel;
+namespace XlsToCsv
+{
+    class ExcelClass
+    {
+        Excel.Application xlApp;
+        Excel.Workbook xlWorbook;
+        Excel._Worksheet xlWorksheet;
+        Excel.Range xlRange;
+        public ExcelClass(string path)
+        {
+            xlApp = new Excel.Application();
+            xlWorbook = xlApp.Workbooks.Open(path);
+            //xlWorksheets = xlWorbook.Sheets.;
+        }
+        public string ExcelToString()
+        {
+            string csvString="";
+            //Lese alle Excelsheets
+            for(int i =1; i<=xlWorbook.Sheets.Count;i++)
+            {
+                xlWorksheet = xlWorbook.Sheets[i];
+                xlRange = xlWorksheet.UsedRange;
+
+                int rowCount = xlRange.Rows.Count;
+                int colCount = xlRange.Columns.Count;
+                //Lese Zeilen
+                for(int j = 1; j<=rowCount;j++ )
+                {
+                    //Lese Spalten
+                    for(int x = 1; x<=colCount; x++)
+                    {
+                        //Wenn Zelle nicht leer ist, lese Zelle ein
+                        //Ansonsten Füge leere Zelle ein
+                        if(xlRange.Cells[j, x] != null && xlRange.Cells[j, x].Value2 != null)
+                        {
+                            csvString += xlRange.Cells[j, x].Value2.ToString() + ";";
+                        }
+                        else
+                        {
+                            csvString += ";";
+                        }
+                        
+                    }
+                    //Neue Zeile
+                    csvString += "\n";
+                }
+                //Vor dem nächsten Sheet 2 Leerzeilen
+                csvString += "\n\n";
+            }
+            //Garbage Collector Reinigung
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            //Schliese ExcelFile
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            xlWorbook.Close();
+            Marshal.ReleaseComObject(xlWorbook);
+
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+            //Rückgabe
+            return csvString;
+        }
+        public bool StringToCsv(string path, string csvString)
+        {
+            string NewPath = path;
+            var NP = NewPath.ToCharArray();
+            //In der Verzeichnisstruktur darf lediglich "1" Punkt bei der Dateiendung
+            //vorhanden sein
+            if (NP.Where(x=>x.Equals('.')).Count()==1)
+            {
+                NewPath = NewPath.Split('.')[0];
+                NewPath += ".csv";
+
+                //Datei erstellen und schreiben
+                try
+                {
+                    using (FileStream fs = File.Create(NewPath))
+                    {
+                        byte[] csvText = new UTF8Encoding(true).GetBytes(csvString);
+                        fs.Write(csvText,0,csvText.Length);
+                    }
+                }
+                catch(Exception e)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ XlsToCsv/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace XlsToCsv
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConvert_Click(object sender, RoutedEventArgs e)
+        {
+            //Konvertiere Datei
+            string file = tbxPfad.Text;
+            ExcelClass excel = new ExcelClass(file);
+            string csvString = excel.ExcelToString();
+            if (excel.StringToCsv(file, csvString))
+            {
+                MessageBox.Show("Konvertierung erfolgreich");
+            }
+            else
+            {
+                MessageBox.Show("Es ist ein Fehler aufgetreten\nBitte überprüfen Sie ob sich ein '.' in der Ordnerstruktur befindet", "Fehler", MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+
+        }
+
+        private void BtnAbbrechen_Click(object sender, RoutedEventArgs e)
+        {
+            //entferne Datei
+            tbxPfad.Text = "";
+        }
+
+        private void BntSucheFile_Click(object sender, RoutedEventArgs e)
+        {
+            //Öffne Datei
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "New Excel Files (*.xlxs)|*.xlsx";//|Old Excel Files (*.xls)|*.xls
+            if (ofd.ShowDialog()==true)
+            {
+                tbxPfad.Text = ofd.FileName;
+            }
+        }
+
+        private void TbxPfad_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //Aktiviere Button nur wenn eine Datei ausgewählt wurde
+            btnConvert.IsEnabled = (!tbxPfad.Text.Equals(""));
+        }
+
+    }
+}
